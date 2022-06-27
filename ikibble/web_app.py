1c97406d6582695882ba5b748680e2116995c514
robert-drop-table <85031828+robert-drop-table@users.noreply.github.com>
2022-06-27 21:16:55 +0100

    Changed to classic Flask app folder sctructure

--- ikibble/web_app.py
+++ ikibble/web_app.py
@@ -3,10 +3,7 @@
 import os
 from flask import Flask, render_template, url_for
 
-template_path = os.path.abspath('../templates/')
-static_path = os.path.abspath('../static/')
-
-app = Flask(__name__, template_folder=template_path, static_folder=static_path)
+app = Flask(__name__)
 
 
 @app.route('/')
