77ce4f61d9df9e0998ab8ddc12e06955d16b1eca
robert-drop-table <85031828+robert-drop-table@users.noreply.github.com>
2022-06-28 23:34:03 +0100

    Adjusted nav bar spacing

--- ikibble/web_app.py
+++ ikibble/web_app.py
@@ -10,5 +10,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/kibble_options')
+def index():
+    return render_template('index.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
